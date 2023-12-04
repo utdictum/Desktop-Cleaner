5ae6331d2f95b3911b6349898f36323c9225fc34
utdictum <natgeomara@gmail.com>
2023-12-04 18:23:21 +0200

    moving files

--- sort_desktop.py
+++ sort_desktop.py
@@ -5,7 +5,6 @@ import shutil
 file_mappings = {
     '.jpg': 'Images',
     '.png': 'Images',
-    '.gif': 'Images',
     '.pdf': 'Documents',
     '.txt': 'Documents',
     '.docx': 'Documents',
@@ -16,14 +15,13 @@ file_mappings = {
     '.ait': 'Illustrator',
     '.indd': 'InDesign',
     '.indt': 'InDesign',
-    '.prproj': 'PremierePro',
     '.aep': 'AfterEffects',
     '.aet': 'AfterEffects',
     '.lrtemplate': 'Lightroom',
     '.lrcat': 'Lightroom',
-    '.pdf': 'Acrobat',
     '.cdr': 'CorelDRAW',
     '.cdt': 'CorelDRAW',
+    '.pdf': 'Acrobat',
     '.cpt': 'CorelPhotoPaint',
     '.rif': 'CorelPainter',
     '.pat': 'CorelPainter',
@@ -32,6 +30,11 @@ file_mappings = {
 #os.path.expanduser('~') find the home directory of the current user, ~ being shorthand for home directory
 #os.path.join joins home directory with desktop so its independent from windows 
 desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
-#os.listdir(desktop_path) list all in the file and directories in the constructed desktop directory
+#os.listdir(desktop_path) list all the file and directories in the constructed desktop directory
 #os.path.isfile(os.path.join(desktop_path, f)) checks if file then joins constructed desktop directory to the f if file
 files = [f for f in os.listdir(desktop_path) if os.path.isfile(os.path.join(desktop_path, f))]
+
+
+# MOVING FILES
+for file in files:
+    file_extension = os.path.splitext(file)[1]
