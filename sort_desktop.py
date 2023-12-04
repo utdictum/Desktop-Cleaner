2ab78c314cb0d1ebfcb6c7101d197646d9c7f003
utdictum <natgeomara@gmail.com>
2023-12-04 19:00:30 +0200

    intialised file location before calling

--- sort_desktop.py
+++ sort_desktop.py
@@ -3,8 +3,8 @@ import shutil
 
 
 file_mappings = {
-    '.jpg': 'Images',
-    '.png': 'Images',
+    '.jpg': 'ImagesJPG',
+    '.png': 'ImagesPNG',
     '.pdf': 'Documents',
     '.txt': 'Documents',
     '.docx': 'Documents',
@@ -30,6 +30,7 @@ file_mappings = {
 #os.path.expanduser('~') find the home directory of the current user, ~ being shorthand for home directory
 #os.path.join joins home directory with desktop so its independent from windows 
 desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+
 #os.listdir(desktop_path) list all the file and directories in the constructed desktop directory
 #os.path.isfile(os.path.join(desktop_path, f)) checks if file then joins constructed desktop directory to the f if file
 files = [f for f in os.listdir(desktop_path) if os.path.isfile(os.path.join(desktop_path, f))]
@@ -50,6 +51,8 @@ for file in files:
         #construct file to current location and new     
         file_current_location = os.path.join(desktop_path, file)
         file_new_location = os.path.join(folder_path, file)
+        #need to initialise new_file_location with file_new_location
+        new_file_location = file_new_location
         #function to move form argument mapping current location to new
         ##FIX FOR OVERWRITING ISSUE
         if os.path.exists(file_new_location):
@@ -59,8 +62,7 @@ for file in files:
             new_file_location = os.path.join(folder_path, new_file_name)
             while os.path.exists(new_file_location):
                 i += 1
-            new_file_name = f"{base}_{i}{extension}"
-            new_file_location = os.path.join(folder_path, new_file_name)
+                new_file_name = f"{base}_{i}{extension}"
+                new_file_location = os.path.join(folder_path, new_file_name)
 
-        file_new_location = new_file_location
         shutil.move(file_current_location, file_new_location)
