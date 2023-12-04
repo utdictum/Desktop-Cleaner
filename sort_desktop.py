c3cc4a757b75e16dde5c3d82c03b16397b955044
utdictum <natgeomara@gmail.com>
2023-12-04 19:01:47 +0200

    FIXED case sensitivity

--- sort_desktop.py
+++ sort_desktop.py
@@ -39,7 +39,7 @@ files = [f for f in os.listdir(desktop_path) if os.path.isfile(os.path.join(desk
 # MOVING FILES
 for file in files:
     #extract file extension based on name
-    file_extension = os.path.splitext(file)[1]
+    file_extension = os.path.splitext(file)[1].lower()
 
     if file_extension in file_mappings:
         #if file in extension in mapping retainn folder name that matches files type
