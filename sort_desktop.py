04fc7b93382696868d91f4ceddaac071438a5414
utdictum <natgeomara@gmail.com>
2023-12-04 17:57:00 +0200

    file ampping for sorting

---
+++ sort_desktop.py
@@ -0,0 +1,31 @@
+import os 
+import shutil
+
+
+file_mappings = {
+    '.jpg': 'Images',
+    '.png': 'Images',
+    '.gif': 'Images',
+    '.pdf': 'Documents',
+    '.txt': 'Documents',
+    '.docx': 'Documents',
+    '.psd': 'Photoshop',
+    '.psb': 'Photoshop',
+    '.abr': 'Photoshop',
+    '.ai': 'Illustrator',
+    '.ait': 'Illustrator',
+    '.indd': 'InDesign',
+    '.indt': 'InDesign',
+    '.prproj': 'PremierePro',
+    '.aep': 'AfterEffects',
+    '.aet': 'AfterEffects',
+    '.lrtemplate': 'Lightroom',
+    '.lrcat': 'Lightroom',
+    '.pdf': 'Acrobat',
+    '.cdr': 'CorelDRAW',
+    '.cdt': 'CorelDRAW',
+    '.cpt': 'CorelPhotoPaint',
+    '.rif': 'CorelPainter',
+    '.pat': 'CorelPainter',
+}
+
